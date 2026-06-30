4697426bf3492920d0e17b7d43cbc50fb45272fc
Marcelo Afonso <marcelo@ebi.ac.uk>
2026-06-30 13:57:51 +0100

    minify and version added to files

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,23 @@
 // vite.config.ts
 import { defineConfig } from 'vite';
+import pkg from './package.json';
 
 export default defineConfig({
   build: {
     sourcemap: true,
     emptyOutDir: true,
+    minify: 'terser',
+    terserOptions: {
+      compress: true,
+      mangle: true,
+      format: {
+        comments: false,
+      },
+    },
     lib: {
       entry: 'src/index.js',
       name: 'ProtvistaPdb',
-      fileName: (format) => `protvista-pdb.${format}.js`,
+      fileName: (format) => `protvista-pdb-${pkg.version}.${format}.min.js`,
       formats: ['es', 'umd'],
     },
     rollupOptions: {
