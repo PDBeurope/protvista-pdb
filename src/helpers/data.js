689b9760385b6833deb7a885ebf598cf1bf2a211
Marcelo Afonso <marcelo@ebi.ac.uk>
2026-07-06 16:20:20 +0100

    better embedded API processing

--- src/helpers/data.js
+++ src/helpers/data.js
@@ -66,46 +66,86 @@ class DataHelper {
     }
 
     async processMutlplePDBeApiData() {
-        let pdbeApiUrls = this.getPDBePVApiUrls();
-        await Promise.all(pdbeApiUrls.map(url => fetch(url)))
-        .then(resp => Promise.all( resp.map((r) => { return r.json();}) ))
-        .then(results => {
+    const pdbeApiUrls = this.getPDBePVApiUrls();
+
+    const results = await Promise.all(
+      pdbeApiUrls.map(async (url) => {
+        try {
+          const response = await fetch(url);
+
+          if (!response.ok) {
+            console.warn(
+              `API unavailable: ${url}`,
+              response.status,
+              response.statusText,
+            );
+            return null;
+          }
+
+          return await response.json();
+        } catch (err) {
+          console.warn(`API unavailable: ${url}`, err);
+          return null;
+        }
+      }),
+    );
+
             results.forEach((result, resultIndex) => {
-                if(typeof result == 'undefined' || result == null || Object.keys(result).length === 0){
-                    //skip result    
-                }else{
+      if (!result || Object.keys(result).length === 0) {
+        return;
+      }
                  
                     let resultKey = this.entryId ? this.entryId : this.accession;
-                    if(pdbeApiUrls[resultIndex].split('/').indexOf('uniprot_mapping') > -1){
-                        if(result[resultKey].tracks && result[resultKey].tracks.length == 1){
+
+      if (pdbeApiUrls[resultIndex].split("/").indexOf("uniprot_mapping") > -1) {
+        if (
+          result[resultKey]?.tracks &&
+          result[resultKey].tracks.length === 1
+        ) {
                             this.accession = result[resultKey].tracks[0].data[0].label;
+          resultKey = this.accession;
                         }
                     }
 
                     if (!result[resultKey]) return;
-                    if (result[resultKey].displayNavigation && result[resultKey].displayNavigation == false) this.viewerData.displayNavigation = false;
-                    if (result[resultKey].displaySequence && result[resultKey].displaySequence == false) this.viewerData.displaySequence = false;
-                    if (result[resultKey].sequence) this.viewerData.sequence = result[resultKey].sequence;
-                    if (result[resultKey].length) this.viewerData.length = result[resultKey].length;
-                    if (result[resultKey].tracks) this.viewerData.tracks = this.viewerData.tracks.concat(result[resultKey].tracks);
-                    if (result[resultKey].legends){
-                        if (result[resultKey].legends.alignment) this.viewerData.legends.alignment = result[resultKey].legends.alignment;
+
+      if (result[resultKey].displayNavigation === false) {
+        this.viewerData.displayNavigation = false;
+      }
+
+      if (result[resultKey].displaySequence === false) {
+        this.viewerData.displaySequence = false;
+      }
+
+      if (result[resultKey].sequence) {
+        this.viewerData.sequence = result[resultKey].sequence;
+      }
+
+      if (result[resultKey].length) {
+        this.viewerData.length = result[resultKey].length;
+      }
+
+      if (result[resultKey].tracks) {
+        this.viewerData.tracks = this.viewerData.tracks.concat(
+          result[resultKey].tracks,
+        );
+      }
+
+      if (result[resultKey].legends) {
+        if (result[resultKey].legends.alignment) {
+          this.viewerData.legends.alignment =
+            result[resultKey].legends.alignment;
+        }
                         
-                        if (result[resultKey].legends.data){ 
-                            for(let legendKey in result[resultKey].legends.data){
-                                this.viewerData.legends.data[legendKey] = result[resultKey].legends.data[legendKey];
+        if (result[resultKey].legends.data) {
+          for (const legendKey in result[resultKey].legends.data) {
+            this.viewerData.legends.data[legendKey] =
+              result[resultKey].legends.data[legendKey];
                             }
                         }
-
-                    }
-                    
                 }
-
             });
         
-        }, err => {
-            console.log(`API unavailable!`, e);
-        });
 
         return this.viewerData;
     }
